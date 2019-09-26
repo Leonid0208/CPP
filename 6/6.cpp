795ad692ac61e25b806de423404d0ddd5246be26
Leonid0208 <themusik.batbka@gmail.com>
2019-09-26 14:09:37 +0300

    Добавил 6 задание

---
+++ 6/6.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+using namespace std;
+#include <math.h> // подключаем билиотеку 
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b, c, x, d, x1, x2, s;
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	cout << "c = ";
+	cin >> c;
+	d = b * b - 4 * a * c;
+	if (d > 0 & a!=0 && b !=0) {
+		s = sqrt(d);
+		x1 = (-b - s) / 2 * a;
+		x2 = (-b + s) / 2 * a;
+		cout << "x1 = " << x1 << endl;
+		cout << "x2 = " << x2;
+
+	}
+	else if (d = 0)
+		cout << "x = " << (-b / 2 * a);
+	else if (a == 0 && b != 0)
+		cout << "x = " << (-c / b);
+	else if (a != 0 && b == 0)
+		cout << "x = " << (sqrt(-c/a));
+	else
+		cout << "Решений нет";
+
+
+
+}
