142b933344166af986146c0e034cc05f68d1b9d3
Leonid0208 <themusik.batbka@gmail.com>
2019-09-29 20:15:40 +0300

    Поправил решение 2 и 6 задачи

--- 2/2.cpp
+++ 2/2.cpp
@@ -1,9 +1,37 @@
-﻿#include <iostream>
-int main() {
-	int a = 3, b = 3.14;
-	double c = 3, d = 3.14;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl; //выведет 3 потому, что переменная b - целочисленная 
-	std::cout << c << std::endl;
-	std::cout << d; //выведет 3.14 
+﻿
+#include <iostream>
+using namespace std;
+#include <math.h> // подключаем билиотеку 
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b, c, x, d, x1, x2, s;
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	cout << "c = ";
+	cin >> c;
+	d = b * b - 4 * a * c;
+	if (d > 0 && a != 0 && b != 0) {
+		s = sqrt(d);
+		x1 = (-b - s) / 2 / a;
+		x2 = (-b + s) / 2 / a;
+		cout << "x1 = " << x1 << endl;
+		cout << "x2 = " << x2;
+
+	}
+	else if (d == 0)
+		cout << "x = " << (-b / 2 / a);
+	else if (a == 0 && b != 0)
+		cout << "x = " << (-c / b);
+	else if ((a < 0 && b == 0 && c >0) || (a > 0 && b == 0 && c < 0))
+		cout << "x = " << (sqrt(-c / a));
+	else if (c == 0 && a == 0 && b == 0)
+		cout << "Бесконечно много решений";
+	else
+		cout << "Решений нет";
+
+
+
 }
--- 6/6.cpp
+++ 6/6.cpp
@@ -13,20 +13,22 @@ int main()
 	cout << "c = ";
 	cin >> c;
 	d = b * b - 4 * a * c;
-	if (d > 0 & a!=0 && b !=0) {
+	if (d > 0 && a != 0 && b != 0) {
 		s = sqrt(d);
-		x1 = (-b - s) / 2 * a;
-		x2 = (-b + s) / 2 * a;
+		x1 = (-b - s) / 2 / a;
+		x2 = (-b + s) / 2 / a;
 		cout << "x1 = " << x1 << endl;
 		cout << "x2 = " << x2;
 
 	}
-	else if (d = 0)
-		cout << "x = " << (-b / 2 * a);
+	else if (d == 0)
+		cout << "x = " << (-b / 2 / a);
 	else if (a == 0 && b != 0)
 		cout << "x = " << (-c / b);
 	else if (a != 0 && b == 0)
-		cout << "x = " << (sqrt(-c/a));
+		cout << "x = " << (sqrt(-c / a));
+	else if (a == 0 && b == 0 && c == 0)
+		cout << "x - любое число";
 	else
 		cout << "Решений нет";
 
