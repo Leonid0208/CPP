d662508ce0b93a6fd38423a5ee5cf72f5b041bc6
Leonid0208 <themusik.batbka@gmail.com>
2019-12-07 18:50:14 +0300

    Дополнение к лабе

---
+++ Лабораторные работы/Лабораторная работа №2/Laba2/Laba2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUSSIAN");
+	int i;
+	double a, b, Xs, Xe, Dx, x, par, min, max;
+	cout << "Введите a" << endl;
+	cin >> a;
+	cout << "Введите b" << endl;
+	cin >> b;
+	cout << "Введите Xs" << endl;
+	cin >> Xs;
+	cout << "Введите Xe" << endl;
+	cin >> Xe;
+	cout << "Введите Dx" << endl;
+	cin >> Dx;
+	
+	x = Xs;
+	i = 1;
+	min = exp(x) / (3 + sin(x));
+	max = exp(x) / (3 + sin(x));
+	while (x <= Xe) {
+		if (x <= a) {
+			par = exp(x);
+		}
+		else if (a < x && x < b) {
+			par = exp(x) + cos(x);
+		}
+		else {
+			par = tan(x);
+		}
+		if (par < min) {
+			min = par;
+		}
+		if (par > max) {
+			max = par;
+		}
+		cout << i << ": " << par << endl;
+		i += 1;
+		x += Dx;
+	}
+	cout << "max = " << max << endl;
+	cout << "min = " << min << endl;
+		
+
+
+
+}
